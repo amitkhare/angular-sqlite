24b06ef3af2db122416d36dbf9fbd3993b467e6e
Joel Chu <joelckchu@gmail.com>
2015-03-16 11:29:37 +0800

    update the bower and angular version

--- Sqlite.js
+++ Sqlite.js
@@ -4,17 +4,24 @@
 @toc
 
 */
+(function() 
+{
+    'use strict';
 
-'use strict';
+    angular.module('angularSqlite', [])
+    .factory('nbSqlite', [ function () 
+    {
 
-angular.module('joelchu.angularSqlite', [])
-.factory('nbSqlite', [ function () {
+        //public methods & properties
+        var self ={
+        };
 
-	//public methods & properties
-	var self ={
-	};
-	
-	//private methods and properties - should ONLY expose methods and properties publicly (via the 'return' object) that are supposed to be used; everything else (helper methods that aren't supposed to be called externally) should be private.
-	
-	return self;
-}]);
+        //private methods and properties - should ONLY expose methods and properties publicly (via the 'return' object) that are supposed to be used; everything else (helper methods that aren't supposed to be called externally) should be private.
+
+        return self;
+    }]);
+    
+    
+    
+    
+}());
